370010e3a2f28aebb3b367dcb2069b25daf272af
rAel-Ael <alediru9267@gmail.com>
2026-03-24 15:11:45 +0100

    inizio

---
+++ script.js
@@ -0,0 +1,5 @@
+//intanto prima voglio trovare i numeri da 1 a 100
+// poi per ogni numero se e' divisibile per 3 scrivo "Fizz"
+// per ogni numero divisibile per 5 scrivo "Buzz"
+// se lo sono per entrambi metto "FizzBuzz"
+
