b7f2e668d4f10625b9147c56a69d4f935db61494
rAel-Ael <alediru9267@gmail.com>
2026-03-24 15:32:55 +0100

    esercizio fatto, funziona

--- script.js
+++ script.js
@@ -3,3 +3,20 @@
 // per ogni numero divisibile per 5 scrivo "Buzz"
 // se lo sono per entrambi metto "FizzBuzz"
 
+for (let numb = 1; numb <= 100; numb++) {
+    if (numb % 3 === 0 && numb % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (numb % 3 === 0) {
+        console.log("Fizz");
+    } else if (numb % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(numb);
+        
+}
+
+    
+} 
+
+
+// <= - minore uguale,  && - and, % - modulo per capire se il n e' divisibile, numb - variabile usata per i numeri
